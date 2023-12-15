f0c944f5d76254525505dfe35ae5faaf82be4087
Ryan Lilleyman <113410396+RyanLilleyman@users.noreply.github.com>
2023-12-15 12:51:00 -0500

    Update CardSeeder.php

--- database/seeders/CardSeeder.php
+++ database/seeders/CardSeeder.php
@@ -22,8 +22,14 @@ class CardSeeder extends Seeder
          * create a random number of decks and then a random number of cards for each deck and associate each new
          * card with the deck uuid.
          */
+        
+        // Sets the deck count to a random integer between 10 and 15.
         $deck_count = rand(10,15);
+        
+        // Creates from a factory for each count above
         $decks = Deck::factory()->count($deck_count)->create();
+        
+        // For each deck create a new card associated by the deck is to the id table in the deck model. 
         $decks->each(function ($deck) {
             $cardCount = rand(10, 20);
             Card::factory()->count($cardCount)->create(['deck_id' => $deck->id]);
